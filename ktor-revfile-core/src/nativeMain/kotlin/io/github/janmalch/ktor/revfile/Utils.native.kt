f662b90c15ebb02a92708030fc415c1904dcb608
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-07-19 15:56:38 +0200

    fix: implement internal hashing for native platforms

--- ktor-revfile-core/src/nativeMain/kotlin/io/github/janmalch/ktor/revfile/Utils.native.kt
+++ ktor-revfile-core/src/nativeMain/kotlin/io/github/janmalch/ktor/revfile/Utils.native.kt
@@ -1,12 +1,24 @@
 package io.github.janmalch.ktor.revfile
 
-import io.ktor.http.content.*
+import io.ktor.http.content.OutgoingContent
+import io.ktor.utils.io.readAvailable
+import kotlinx.coroutines.runBlocking
 import okio.ByteString
 import okio.HashingSink.Companion.sha256
 import okio.blackholeSink
+import okio.buffer
 import okio.use
 
 internal actual fun OutgoingContent.ReadChannelContent.digestSha256(): ByteString =
     sha256(blackholeSink()).use { hashingSink ->
-        TODO() // FIXME
+        hashingSink.buffer().use { sink ->
+            val channel = readFrom()
+            do {
+                val byteArray = ByteArray(1024)
+                val currentRead = runBlocking { channel.readAvailable(byteArray)  }
+                sink.write(byteArray)
+            } while (currentRead > 0)
+        }
+        hashingSink.hash
     }
+
